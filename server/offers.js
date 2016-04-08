099790e34bb3a02e67cb72889869541c029238d4
Xinxi Chen <xc336@cornell.edu>
2016-04-07 21:27:33 -0400

    add posted time ago feature

--- server/offers.js
+++ server/offers.js
@@ -7,6 +7,44 @@ var db = require('./pghelper'),
 //
 // };
 //
+
+function timeSince(date) {
+    var seconds = Math.floor((new Date() - date) / 1000);
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    if (seconds < 5){
+        return "just now";
+    }else if (seconds < 60){
+        return seconds + " seconds ago";
+    }
+    else if (seconds < 3600) {
+        minutes = Math.floor(seconds/60)
+        if(minutes > 1)
+            return minutes + " minutes ago";
+        else
+            return "1 minute ago";
+    }
+    else if (seconds < 86400) {
+        hours = Math.floor(seconds/3600)
+        if(hours > 1)
+            return hours + " hours ago";
+        else
+            return "1 hour ago";
+    }
+    //2 days and no more
+    else if (seconds < 172800) {
+        days = Math.floor(seconds/86400)
+        if(days > 1)
+            return days + " days ago";
+        else
+            return "1 day ago";
+    }
+    else{
+
+        //return new Date(time).toLocaleDateString();
+        return date.getDate().toString() + " " + months[date.getMonth()] + ", " + date.getFullYear();
+    }
+}
+
 function getTime() {
       var d = new Date();
       // var x = offer-detail.getElementById("demo");
@@ -26,6 +64,12 @@ function findById(id) {
     return db.query('SELECT id, sfId, name, startDate, endDate, description, image__c AS image, campaignPage__c AS campaignPage, publishDate__c AS publishDate, deal__valid__duration, createdTime, bartender__id, barName, bartenderName, location, location__latitude__s, location__longitude__s, bartenderPic FROM salesforce.campaign WHERE ' + (isNaN(id) ? 'sfId' : 'id') + '=$1', [id], true);
 };
 
+// function findById(id) {
+//     // Retrieve offer either by Salesforce id or Postgress id
+//     offer = db.query('SELECT id, sfId, name, startDate, endDate, description, image__c AS image, campaignPage__c AS campaignPage, publishDate__c AS publishDate, deal__valid__duration, createdTime, bartender__id, barName, bartenderName, location, location__latitude__s, location__longitude__s, bartenderPic FROM salesforce.campaign WHERE ' + (isNaN(id) ? 'sfId' : 'id') + '=$1', [id], true);
+//     return offer;
+// };
+
 function getAll(req, res, next) {
     findAll(20)
         .then(function (offers) {
@@ -39,11 +83,14 @@ function getById(req, res, next) {
     var id = req.params.id;
     findById(id)
         .then(function (offer) {
+            offer.timeago = timeSince(offer.createdtime);
             console.log(JSON.stringify(offer));
             return res.send(JSON.stringify(offer));
         })
         .catch(next);
 };
+
+
 
 /* Feed Function
 
