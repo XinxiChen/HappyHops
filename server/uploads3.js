fbb5e63258a75590bd6cac14b637afda660a2830
Xinxi Chen <xc336@cornell.edu>
2016-04-20 22:45:54 -0400

    finish upload function and req

--- server/uploads3.js
+++ server/uploads3.js
@@ -1,5 +1,6 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
+var fs = require('fs');
 
 // Set your region for future requests.
 AWS.config.region = 'us-west-2';
@@ -7,9 +8,6 @@ AWS.config.region = 'us-west-2';
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  //
-  // accessKeyId: "AKIAIRMB6JXHG5C3AQBQ",
-  // secretAccessKey: "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"
 });
 
 function upload(req, res, next) {
@@ -19,14 +17,34 @@ function upload(req, res, next) {
   // console.log(req.file.path);
   // Create a bucket using bound parameters and put something in it.
   // Make sure to change the bucket name from "myBucket" to something unique.
+  var bodyStream = fs.createReadStream(req.file.path);
+
   var s3bucket = new AWS.S3({params: {Bucket: 'happyhops/bartenders'}});
   s3bucket.createBucket(function() {
-    var params = {Key: req.file.path, Body: 'Hello!'};
+    var params = {
+      Key: req.file.filename,
+      ACL: 'public-read',
+      Body: bodyStream
+    };
     s3bucket.upload(params, function(err, data) {
       if (err) {
         console.log("Error uploading data: ", err);
       } else {
+        var return_data = {
+          signed_request: data,
+          url: 'https://s3-us-west-2.amazonaws.com/happyhops/bartenders/' + req.file.filename
+        };
         console.log("Successfully uploaded data to happyhops/bartenders");
+        console.log("Going to delete an existing file");
+        fs.unlink(req.file.path, function(err) {
+           if (err) {
+               return console.error(err);
+           }
+           console.log("File deleted successfully!");
+        });
+        console.log(return_data);
+        res.write(JSON.stringify(return_data));
+        res.end();
       }
     });
   });
